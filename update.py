dec9cb45ca6cd5c6d2a59efc090fd8d58f00205b
kraxarn <kraxie@protonmail.com>
2022-02-17 23:13:38 +0100

    Initial version with working downloader

--- update.py
+++ update.py
@@ -1,36 +1,57 @@
 import datetime
 import os
 import sys
-from github import WorkflowRun, Github
+import requests
 
 # Access token needs to be set for GitHub API calls
 if "ACCESS_TOKEN" not in os.environ:
 	sys.exit("Error: Access token not specified")
 
-workflow_ids = {
-	7734249,   # Linux
-	18195390,  # Windows (Qt 6, x64)
-	18401182,  # Windows (Qt 5, x86)
-	18407206,  # macOS
+workflows = {
+	7734249: "spotify-qt-linux-nightly.AppImage",
+	18195390: "spotify-qt-win64-nightly.zip",
+	18401182: "spotify-qt-win32-nightly.zip",
+	18407206: "spotify-qt-macos-nightly.dmg",
 }
 
-api = Github(os.environ["ACCESS_TOKEN"])
+access_token = os.environ["ACCESS_TOKEN"]
 
-build_repo = api.get_repo(458937795)
-source_repo = api.get_repo(239120579)
+headers = {
+	"Authorization": f"token {access_token}",
+}
 
-for workflow_id in workflow_ids:
-	workflow = source_repo.get_workflow(workflow_id)
-	runs = workflow.get_runs()
-	latest_run = source_repo.get_workflow_run(runs[0].id)
-	print(f"Last {workflow.name} run: {latest_run.updated_at}")
+build_repo_name = "kraxarn/spotify-qt-builds"
+source_repo_name = "kraxarn/spotify-qt"
+
+
+def get_latest_artifact_url(workflow_id: int) -> str:
+	artifacts_url = requests \
+		.get(f"https://api.github.com/repos/{source_repo_name}/actions/workflows/{workflow_id}/runs", headers=headers) \
+		.json()["workflow_runs"][0]["artifacts_url"]
+
+	return requests.get(artifacts_url, headers=headers) \
+		.json()["artifacts"][0]["archive_download_url"]
+
+
+def download_file(source: str, target: str):
+	with requests.get(source, headers=headers, stream=True) as response:
+		with open(target, "wb") as file:
+			for chunk in response.iter_content(chunk_size=8192):
+				file.write(chunk)
+
+
+for workflow in workflows:
+	url = get_latest_artifact_url(workflow)
+	filename = workflows[workflow]
+	print(f"[{workflow: <8}] {url} -> {filename}")
+	download_file(url, filename)
 
 # print(f"Builds updated: {build_repo.updated_at}")
 # print(f"Source updated: {source_repo.updated_at}")
 
-if build_repo.updated_at > source_repo.updated_at:
-	print("Already up-to-date")
-	sys.exit()
+# if build_repo.updated_at > source_repo.updated_at:
+# 	print("Already up-to-date")
+# 	sys.exit()
 
 # build_updated_days = (datetime.datetime.now() - build_repo.updated_at).days
 # build_updated_days_suffix = "day" if build_updated_days == 1 else "days"
