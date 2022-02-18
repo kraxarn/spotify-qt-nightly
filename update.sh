540ce0c8176221a8dbd78d482ca0309c23d8f75a
kraxarn <kraxie@protonmail.com>
2022-02-18 21:02:37 +0100

    Less verbose logging

--- update.sh
+++ update.sh
@@ -3,14 +3,11 @@ export HEADER="Authorization: token $ACCESS_TOKEN"
 
 latest_artifact_url() {
 	runs_url="https://api.github.com/repos/$SOURCE_REPO/actions/workflows/$1/runs"
-	echo "Found runs url: $runs_url"
 	artifacts_url=$(curl -s -H "$HEADER" "$runs_url" | jq -r '.workflow_runs[0].artifacts_url')
-	echo "Found artifacts url: $artifacts_url"
 	curl -s -H "$HEADER" "$artifacts_url" | jq -r '.artifacts[0].archive_download_url'
 }
 
 download_file() {
-	echo "Downloading: $1"
 	curl -L -H "$HEADER" "$1" -o "$2"
 }
 
